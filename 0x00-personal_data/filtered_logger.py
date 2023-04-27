7220c1ca26df300c231907119358457a51da6da6
Idreex <idreesademola07@gmail.com>
2023-04-27 21:07:58 +0100

    remove bugs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ import os
 import mysql.connector
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+fields = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -22,6 +22,7 @@ def filter_datum(fields: List[str], redaction: str,
         message (str): the log line to obfuscate
         separator (str): the character separating the fields
     """
+    
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
                          field+'='+redaction+separator, message)
